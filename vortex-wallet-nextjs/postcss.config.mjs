642d388f33ac5d2cee70497cd33d5533b93197b9
opkivi4 <opkivi4@gmail.com>
2025-08-05 20:30:48 +0000

    Changes before Firebase Studio auto-run

--- vortex-wallet-nextjs/postcss.config.mjs
+++ vortex-wallet-nextjs/postcss.config.mjs
@@ -1,5 +1,11 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Эта строка говорит искать классы во всех jsx/tsx файлах в папке src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
